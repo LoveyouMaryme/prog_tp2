9b3e223bd30e2f04f9d1214ce8b531f241226271
lmv <love-mary.victor@outlook.com>
2026-04-05 22:04:38 -0400

    java doc pour carte fouette

--- src/Fouette.java
+++ src/Fouette.java
@@ -1,4 +1,16 @@
+/**
+ * Représente la carte de dommage « Fouette ».
+ *
+ * Inflige 2 points de dommage au joueur adverse.
+ */
+
 public class Fouette extends CarteDommage {
+
+    /**
+     * Construit une carte « Fouette »
+     *
+     * @param idJoueur ID du joueur qui l'utilise
+     */
     public Fouette(int idJoueur) {
         super("Fouette", idJoueur, 2);
     }
