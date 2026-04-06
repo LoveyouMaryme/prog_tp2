5287d5017230fbb79b4f2052703ffd4138a02e10
lmv <love-mary.victor@outlook.com>
2026-04-05 22:51:02 -0400

    java doc pour petitepause et petitvoleur

--- src/PetitVoleur.java
+++ src/PetitVoleur.java
@@ -1,16 +1,39 @@
 import java.util.Deque;
 
+
+/**
+ * Représente la carte d'attaque « Petit Voleur ».
+ *
+ * Diminue le nombre de cartes en main du joueur adverse
+ * d'au plus trois cartes.
+ */
 public class PetitVoleur extends CarteAttaque {
     public PetitVoleur(int idJoueur) {
         super("PetitVoleur", idJoueur);
     }
 
+
+    /**
+     * Applique les effets de deuxième type :
+     * augmente de 1 le nombre de points d'expérience
+     * du joueur courant.
+     *
+     * @param jc joueur courant, celui qui joue la carte
+     * @param ja joueur adverse, celui qui ne joue pas la carte
+     */
     @Override
     public void appliquerEffetType1(Joueur jc, Joueur ja) {
         // Ja.m = max(0, Ja.m - 3)
         ja.m = Math.max(0, ja.m - 3);
     }
 
+    /**
+     * Applique les effets de premier type :
+     * Augmente le nombre d'expériences du joueur ciblé d'un point
+     * @param joueurs   le tableau des deux joueurs
+     * @param pile      la pile de cartes jouées
+     * @param nbRiposte compteur du nombre de cartes de riposte déjà jouées
+     */
     @Override
     public void appliquerEffetType2(Joueur[] joueurs, Deque<Carte> pile, int[] nbRiposte) {
         joueurs[this.idJoueur].e += 1;
--- src/PetitePause.java
+++ src/PetitePause.java
@@ -1,16 +1,44 @@
 import java.util.Deque;
 
+/**
+ * Représente la carte d'attaque « Petite Pause ».
+ *
+ * Modifie le nombre de cartes en main des deux joueurs :
+ * <ol>
+ *  <ul>Un de plus pour le joueur courant.</ul>
+ *  <ul>Un de moins pour le joueur adverse.</ul>
+ * </ol>
+ */
 public class PetitePause extends CarteAttaque {
+
+    /**
+     * Construit une carte « Petite Pause »
+     *
+     * @param idJoueur ID du joueur qui l'utilise
+     */
     public PetitePause(int idJoueur){
         super("PetitePause", idJoueur);
     }
 
+    /**
+     * Applique les effets de premier type :
+     * ajuste le nombre de cartes en main des deux joueurs.
+     *
+     * @param jc joueur courant, celui qui joue la carte
+     * @param ja joueur adverse, celui qui ne joue pas la carte
+     */
     @Override
     public void appliquerEffetType1(Joueur jc, Joueur ja){
         jc.m = jc.m + 1;
-        ja.m = ja.m + 1;
+        ja.m = ja.m - 1;
     }
 
+    /**
+     * Applique les effets de premier type pour ce type précis de carte d'Attaque
+     * @param joueurs   le tableau des deux joueurs
+     * @param pile      la pile de cartes jouées
+     * @param nbRiposte compteur du nombre de cartes de riposte déjà jouées
+     */
     @Override
     public void appliquerEffetType2(Joueur[] joueurs, Deque<Carte> pile, int[] nbRiposte){
         //aucun effet
