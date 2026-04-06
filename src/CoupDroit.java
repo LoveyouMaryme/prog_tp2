f3407139fb29eeca46286080f8cce1a304717d7a
lmv <love-mary.victor@outlook.com>
2026-04-05 21:50:59 -0400

    java doc pour coup droit

--- src/CoupDroit.java
+++ src/CoupDroit.java
@@ -1,3 +1,8 @@
+/**
+ * Représente la carte de dommage « Coup droit ».
+ *
+ * Inflige 1 point de dommage au joueur adverse.
+ */
 public class CoupDroit extends CarteDommage {
     public CoupDroit(int idJoueur){
         super("CoupDroit", idJoueur, 1);
